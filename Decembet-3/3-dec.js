772c92b98249f693522a958a64f60255135cc2ab
pa1-shah <pawanshah403@gmail.com>
2024-12-03 13:26:25 +0530

    day 3 first part

---
+++ Decembet-3/3-dec.js
@@ -0,0 +1,26 @@
+function checkForCorrectMul(s) {
+  const regex = /mul\((-?\d+(\.\d+)?),(-?\d+(\.\d+)?)\)/g;
+  let i = 0;
+  let sum = 0;
+  const matches = s.matchAll(regex);
+  for (let match of matches) {
+    const firstNum = parseFloat(match[1]);
+    const secNum = parseFloat(match[3]);
+    const product = firstNum * secNum;
+    sum += product;
+  }
+  return sum;
+}
+
+const fs = require("fs");
+
+function fileReadfromText() {
+  fs.readFile("dec3.txt", "utf-8", (err, data) => {
+    if (err) {
+      throw err;
+    }
+    let fileContent = data;
+    console.log(checkForCorrectMul(fileContent));
+  });
+}
+fileReadfromText();
